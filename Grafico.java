531abc996a325c011a50787b6cdbd81f42df40c1
Damacy64 <pzx6464@gmail.com>
2022-09-20 22:01:46 -0500

    agregando atributos

--- Grafico.java
+++ Grafico.java
@@ -16,6 +16,15 @@ public class Grafico extends JFrame
         implements ActionListener //PaolaNeri ClaseGraficacion 2020
 {
 
+    int xP[] = {100, 200, 350, 500, 600, 600, 500, 200, 100, 100};
+    int yP[] = {50, 150, 50, 150, 50, 300, 400, 400, 300, 50};
+    int trianguloX[] = {300, 400, 350};
+    int trianguloY[] = {400, 400, 450};
+    int hojaIzqX[] = {350, 350, 300, 250, 250, 300, 350};
+    int hojaIzqY[] = {600, 550, 500, 500, 550, 600, 600};
+    int hojaDerX[] = {350, 400, 500, 500, 450, 350, 350};
+    int hojaDerY[] = {650, 650, 550, 500, 500, 600, 650};
+
     JMenuBar menu = new JMenuBar(); //crea una barra de menu
     JMenu menu1 = new JMenu("Entramado", true);//Submenu	
     JMenuItem it1 = new JMenuItem("Iniciar");
@@ -37,8 +46,8 @@ public class Grafico extends JFrame
     public Grafico() throws IOException//crear el constructor del frame
     {
         super("Entramado");//Asi se llama al constructor de una clase extendida
-        maxX = 1250;
-        maxY = 800;
+        maxX = 1366;
+        maxY = 766;
         setJMenuBar(menu);//Agrega la barra de menu al frame
         menu.add(menu1);//agrega los menus 1 y 3 a la barra de menu
         menu1.add(it1);// agrega items al menu 1 y 
@@ -79,10 +88,10 @@ public class Grafico extends JFrame
         if (object == it4) {
             moverX(lienzo.getGraphics());
         }
-        if (object == it5){
+        if (object == it5) {
             moverY(lienzo.getGraphics());
         }
-        if (object == it6){
+        if (object == it6) {
             moverXY(lienzo.getGraphics());
         }
     }
@@ -90,19 +99,11 @@ public class Grafico extends JFrame
     public void dibuja(Graphics g) {
         g.clearRect(0, 0, maxX, maxY);
         g.setColor(Color.pink);
-        int xP[] = {100, 200, 350, 500, 600, 600, 500, 200, 100, 100};
-        int yP[] = {50, 150, 50, 150, 50, 300, 400, 400, 300, 50};
         g.fillPolygon(xP, yP, 10);
-        int trianguloX[] = {300, 400, 350};
-        int trianguloY[] = {400, 400, 450};
         g.setColor(Color.GREEN);
         g.fillPolygon(trianguloX, trianguloY, 3);
         g.drawLine(350, 450, 350, 750);
-        int hojaIzqX[] = {350, 350, 300, 250, 250, 300, 350};
-        int hojaIzqY[] = {600, 550, 500, 500, 550, 600, 600};
         g.fillPolygon(hojaIzqX, hojaIzqY, 7);
-        int hojaDerX[] = {350, 400, 500, 500, 450, 350, 350};
-        int hojaDerY[] = {650, 650, 550, 500, 500, 600, 650};
         g.fillPolygon(hojaDerX, hojaDerY, 7);
         g.setColor(Color.BLACK);
         g.drawLine(250, 500, 350, 600);
@@ -112,28 +113,21 @@ public class Grafico extends JFrame
     public void moverX(Graphics g) {
         g.clearRect(0, 0, maxX, maxY);
         Thread t = new Thread();
-        int xP[] = {100, 200, 350, 500, 600, 600, 500, 200, 100, 100};
-        int yP[] = {50, 150, 50, 150, 50, 300, 400, 400, 300, 50};
-        int trianguloX[] = {300, 400, 350};
-        int trianguloY[] = {400, 400, 450};
-        int hojaIzqX[] = {350, 350, 300, 250, 250, 300, 350};
-        int hojaIzqY[] = {600, 550, 500, 500, 550, 600, 600};
-        int hojaDerX[] = {350, 400, 500, 500, 450, 350, 350};
-        int hojaDerY[] = {650, 650, 550, 500, 500, 600, 650};
         try {
-            for (int i = 0,aux=50; i < maxX; i++,aux+=50) {
+            t.start();
+            for (int i = 0, aux = 10; i < maxX - 600; i+=10, aux += 10) {
                 g.clearRect(0, 0, maxX, maxY);
                 for (int j = 0; j < xP.length; j++) {
-                    xP[j] += 50;
+                    xP[j] += 10;
                 }
                 for (int j = 0; j < trianguloX.length; j++) {
-                    trianguloX[j] += 50;
+                    trianguloX[j] += 10;
                 }
                 for (int j = 0; j < hojaIzqX.length; j++) {
-                    hojaIzqX[j] += 50;
+                    hojaIzqX[j] += 10;
                 }
                 for (int j = 0; j < hojaDerX.length; j++) {
-                    hojaDerX[j] += 50;
+                    hojaDerX[j] += 10;
                 }
                 g.setColor(Color.PINK);
                 g.fillPolygon(xP, yP, 10);
@@ -152,19 +146,12 @@ public class Grafico extends JFrame
         }
     }
 
-    public void moverY(Graphics g){
+    public void moverY(Graphics g) {
         g.clearRect(0, 0, maxX, maxY);
         Thread t = new Thread();
-        int xP[] = {100, 200, 350, 500, 600, 600, 500, 200, 100, 100};
-        int yP[] = {50, 150, 50, 150, 50, 300, 400, 400, 300, 50};
-        int trianguloX[] = {300, 400, 350};
-        int trianguloY[] = {400, 400, 450};
-        int hojaIzqX[] = {350, 350, 300, 250, 250, 300, 350};
-        int hojaIzqY[] = {600, 550, 500, 500, 550, 600, 600};
-        int hojaDerX[] = {350, 400, 500, 500, 450, 350, 350};
-        int hojaDerY[] = {650, 650, 550, 500, 500, 600, 650};
         try {
-            for (int i = 0,aux=10; i < maxY; i++,aux+=10) {
+            t.start();
+            for (int i = 0, aux = 10; i < maxY - 500; i+=10, aux += 10) {
                 g.clearRect(0, 0, maxX, maxY);
                 for (int j = 0; j < yP.length; j++) {
                     yP[j] += 10;
@@ -194,36 +181,29 @@ public class Grafico extends JFrame
             System.out.println(e);
         }
     }
-    
-    public void moverXY(Graphics g){
+
+    public void moverXY(Graphics g) {
         g.clearRect(0, 0, maxX, maxY);
         Thread t = new Thread();
-        int xP[] = {100, 200, 350, 500, 600, 600, 500, 200, 100, 100};
-        int yP[] = {50, 150, 50, 150, 50, 300, 400, 400, 300, 50};
-        int trianguloX[] = {300, 400, 350};
-        int trianguloY[] = {400, 400, 450};
-        int hojaIzqX[] = {350, 350, 300, 250, 250, 300, 350};
-        int hojaIzqY[] = {600, 550, 500, 500, 550, 600, 600};
-        int hojaDerX[] = {350, 400, 500, 500, 450, 350, 350};
-        int hojaDerY[] = {650, 650, 550, 500, 500, 600, 650};
         try {
-            for (int i = 0,aux=10,aux1=50; i < maxY; i++,aux+=10,aux1+=50) {
+            t.start();
+            for (int i = 0, aux = 10, aux1 = 10; i < maxY - 500; i+=10, aux += 10, aux1 += 10) {
                 g.clearRect(0, 0, maxX, maxY);
                 for (int j = 0; j < yP.length; j++) {
                     yP[j] += 10;
-                    xP[j] += 50;
+                    xP[j] += 10;
                 }
                 for (int j = 0; j < trianguloY.length; j++) {
                     trianguloY[j] += 10;
-                    trianguloX[j] += 50;
+                    trianguloX[j] += 10;
                 }
                 for (int j = 0; j < hojaIzqY.length; j++) {
                     hojaIzqY[j] += 10;
-                    hojaIzqX[j] += 50;
+                    hojaIzqX[j] += 10;
                 }
                 for (int j = 0; j < hojaDerY.length; j++) {
                     hojaDerY[j] += 10;
-                    hojaDerX[j] += 50;
+                    hojaDerX[j] += 10;
                 }
                 g.setColor(Color.PINK);
                 g.fillPolygon(xP, yP, 10);
@@ -241,6 +221,7 @@ public class Grafico extends JFrame
             System.out.println(e);
         }
     }
+
     //manejador de eventos de ventanas windows
     @Override
     public boolean handleEvent(Event e) {	//en caso de dar clic en el tache de la ventana 	
